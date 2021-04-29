fad7bbd7e46674e6de9865da1ff9ecef0cc600c7
prakruthin <49477322+prakruthin@users.noreply.github.com>
2021-04-29 08:11:52 +0530

    Update parse.py

--- project/lib/parse.py
+++ project/lib/parse.py
@@ -6,23 +6,17 @@ import os
 import datetime
 x=[]
 y=[]
-mydir=os.path.join("C:\\project\\report", datetime.datetime.now().strftime('%Y-%m-%d_%H-%M-%S'))
-os.makedirs(mydir)
-mydir_out=mydir + "\\" + "output.txt"
 
-def addToFile(line):
+
+def addToFile(line,mydir_out):
     with open(mydir_out, "a+") as file:
         file.write(line.split(' ')[9] + "\t" +line.split('   ')[-1])
     file.close()
     
 
 class logParse:
-    def parseData(log_file_path,yaml_file_path):
-        regex1='.* ATTACH_REQUEST\(0x41\) for \(ue_id = (\d+)\)'
-        regex2='.* CREATE SESSION REQ message to SPGW for ue_id 0x0000000(\w+)'
-        regex3='.* MODIFY BEARER REQ to SPGW for ue_id = \((\d+)\).*'
-        regex4='.* Received Attach Complete message for ue_id = \((\d+)\)'
-        regex5='.* Authentication complete \(ue_id=0x0000000(\w+)'
+    def parseData(log_file_path,yaml_file_path,mydir_out):
+        regex=['.* ATTACH_REQUEST\(0x41\) for \(ue_id = (\d+)\)','.* CREATE SESSION REQ message to SPGW for ue_id 0x0000000(\w+)','.* MODIFY BEARER REQ to SPGW for ue_id = \((\d+)\).*','.* Received Attach Complete message for ue_id = \((\d+)\)','.* Authentication complete \(ue_id=0x0000000(\w+)']
 
         with open(yaml_file_path, "r") as f:
             data= yaml.load(f, Loader= yaml.FullLoader)
@@ -35,35 +29,35 @@ class logParse:
         for index in range(0,len(ueid)):
             with open(log_file_path,"r")as file:
                 for line in file:
-                    a = re.search(regex1,line)
-                    b = re.search(regex2,line)
-                    c = re.search(regex3,line)
-                    d = re.search(regex4,line)
-                    e = re.search(regex5,line)
+                    a = re.search(regex[0],line)
+                    b = re.search(regex[1],line)
+                    c = re.search(regex[2],line)
+                    d = re.search(regex[3],line)
+                    e = re.search(regex[4],line)
                     if a:
                         aa=a.group(1)
                         if int(aa) == ueid[index]:
-                            addToFile(line)
+                            addToFile(line,mydir_out)
                             count[ueid[index]] = count.get(ueid[index],0) +1
                     if b:
                         bb=b.group(1)
                         if int(bb,16) == ueid[index]:
-                            addToFile(line)
+                            addToFile(line,mydir_out)
                             count[ueid[index]] = count.get(ueid[index],0) +1
                     if c:
                         cc=c.group(1)
                         if int(cc) == ueid[index]:
-                            addToFile(line)
+                            addToFile(line,mydir_out)
                             count[ueid[index]] = count.get(ueid[index],0) +1
                     if d:
                         dd=d.group(1)
                         if int(dd) == ueid[index]:
-                            addToFile(line)
+                            addToFile(line,mydir_out)
                             count[ueid[index]] = count.get(ueid[index],0) +1
                     if e:
                         ee=e.group(1)
                         if int(ee,16) == ueid[index]:
-                            addToFile(line)
+                            addToFile(line,mydir_out)
                             count[ueid[index]] = count.get(ueid[index],0) +1
                 with open(mydir_out, "a+") as file:
                     file.write("\n")
@@ -86,5 +80,8 @@ def main():
     file = "\\" + "Parser Output " + time_now + ".txt"
     export_file = export_file_path + file
     logparse = logParse
-    logparse.parseData(log_file_path,yaml_file_path)
+    mydir=os.path.join("C:\\project\\report", datetime.datetime.now().strftime('%Y-%m-%d_%H-%M-%S'))
+    os.makedirs(mydir)
+    mydir_out=mydir + "\\" + "output.txt"
+    logparse.parseData(log_file_path,yaml_file_path,mydir_out)
 
